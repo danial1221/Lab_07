95a62851f891fc0c6908c8fd59c1bfdae736a202
Danial <danialmujahid98@gmail.com>
2024-11-19 17:52:13 +0500

    Add files via upload

---
+++ lab_Tasks/Task_7/Form1.Designer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Windows.Forms;  // Required for Windows Forms controls like Label, Timer
+
+namespace WallClockApp
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private Label labelTime;
+        private Timer timerClock;
+
+        // Dispose method to clean up resources
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        // Initialize all UI components
+        private void InitializeComponent()
+        {
+            this.labelTime = new System.Windows.Forms.Label();
+            this.timerClock = new System.Windows.Forms.Timer();
+            this.SuspendLayout();
+
+            // labelTime
+            this.labelTime.AutoSize = true;
+            this.labelTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTime.Location = new System.Drawing.Point(12, 9);
+            this.labelTime.Name = "labelTime";
+            this.labelTime.Size = new System.Drawing.Size(230, 37);
+            this.labelTime.TabIndex = 0;
+            this.labelTime.Text = "00:00:00 AM";
+
+            // timerClock
+            this.timerClock.Interval = 1000;  // 1 second interval
+            this.timerClock.Tick += new System.EventHandler(this.timerClock_Tick);
+
+            // Form1
+            this.ClientSize = new System.Drawing.Size(284, 61);
+            this.Controls.Add(this.labelTime);
+            this.Name = "Form1";
+            this.Text = "Wall Clock";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
---
+++ lab_Tasks/Task_7/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WallClockApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Start the timer when the form loads
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timerClock.Start();  // Start the clock timer
+        }
+
+        // Update the label with the current time and date every second
+        private void timerClock_Tick(object sender, EventArgs e)
+        {
+            // Get current time and date
+            DateTime currentTime = DateTime.Now;
+
+            // Format the time and date string
+            labelTime.Text = currentTime.ToString("dddd, MMMM dd, yyyy hh:mm:ss tt");
+        }
+    }
+}
